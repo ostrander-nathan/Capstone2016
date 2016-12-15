c566322eaaefc12bf74b46829f98a543e2f0bd97
Nathan Ostrander <ostrander_nathan@yahoo.com>
2016-12-15 09:30:45 -0600

    edit button issues

--- javascripts/controllers/EditReviewCtrl.js
+++ javascripts/controllers/EditReviewCtrl.js
@@ -1,16 +1,18 @@
 "use strict";
 app.controller("EditReviewCtrl", function($scope, $rootScope, $routeParams, GoogleFactory, $location, UserFactory, ReviewFactory, ProfileFactory) {
+    $scope.newReview = {};
 	let reviewID = $routeParams.reviewId;
+
 	ProfileFactory.getSingleReviewFB(reviewID).then(function(response){
 		$scope.list = response;
 		console.log("response in editReview",response );
+		$scope.newReview = response;
 	});
 	$scope.submit = function() {
-      console.log("$scope.list", $scope.list);
       ReviewFactory.postReview($scope.list, $rootScope.user.uid);
-      ReviewFactory.getReview($scope.list, $rootScope.user.uid).then(function(reviewId) {
+      console.log("$scope.list", $scope.list);
+      // ReviewFactory.getReview($scope.list, $rootScope.user.uid).then(function(reviewId) {
         $location.url("/users/profile");
-        $scope.newReview = {};
-      });
+      
     };
 });
